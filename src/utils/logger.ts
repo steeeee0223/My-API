51ab15d29e96a69f1dccce410785d049d29596e3
steve.yu <steve.yu@lionic.com>
2023-02-09 16:53:15 +0000

    🚧 Modify `AuthService`, setup error handler and logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,17 +1,17 @@
-import pino from "pino";
-import dayjs from "dayjs";
+import pino from 'pino'
+import dayjs from 'dayjs'
 
 const logger = pino({
     transport: {
         target: 'pino-pretty',
         options: {
-            colorize: true
-        }
+            colorize: true,
+        },
     },
     base: {
         pid: false,
     },
     timestamp: () => `,"time":"${dayjs().format()}"`,
-});
+})
 
-export default logger;
+export default logger
