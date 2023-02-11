f908372f91f2b7591dd377a6e89e2aece368ff2f
steve.yu <steve.yu@lionic.com>
2023-02-11 08:56:17 +0000

    ⚙️ Modify commit command & mongodb connection

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -14,12 +14,13 @@ export const connectDB = async ({
 }: ConnectOptions) => {
     try {
         const connection = await mongoose.connect(
-            `mongodb://${host}:${port}/${db}`,
-            {
-                authSource: 'admin',
-                user: user,
-                pass: pass,
-            }
+            `mongodb+srv://${user}:${pass}@cluster0.ibu1l1d.mongodb.net/?retryWrites=true&w=majority`
+            // `mongodb://${host}:${port}/${db}`,
+            // {
+            //     authSource: 'admin',
+            //     user: user,
+            //     pass: pass,
+            // }
         )
         logger.info('Connected to mongodb...')
         return connection
